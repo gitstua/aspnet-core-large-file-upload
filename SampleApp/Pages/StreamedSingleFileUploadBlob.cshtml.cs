32021aa9cbac3f6c82d7a44dbce7d7f85aee6666
Stuart Eggerton (Microsoft) <steggert@microsoft.com>
2020-05-26 20:33:07 +1000

    fix model name

--- SampleApp/Pages/StreamedSingleFileUploadBlob.cshtml.cs
+++ SampleApp/Pages/StreamedSingleFileUploadBlob.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SampleApp.Pages
 {
-    public class StreamedSingleFileUploadPhysicalModel : PageModel
+    public class StreamedSingleFileUploadBlobModel : PageModel
     {
         public void OnGet()
         {
